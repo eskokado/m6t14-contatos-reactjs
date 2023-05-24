3d357b476b1491fc13c2783071042bf95a5d302c
eskokado <eskokado@gmail.com>
2023-05-24 14:25:59 -0300

    refactor: fix baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://kenziehub.herokuapp.com/",
-  timeout: 3000,
-});
+  baseURL: 'https://localhost:3000/',
+  timeout: 3000
+})
