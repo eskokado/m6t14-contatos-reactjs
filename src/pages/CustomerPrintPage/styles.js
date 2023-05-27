84d9b9e1613c6c9e3f511f28cef6558e1eb59ef6
eskokado <eskokado@gmail.com>
2023-05-27 12:40:41 -0300

    feat: create style into CustomerPrintPage

---
+++ src/pages/CustomerPrintPage/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background: none;
+  border: none;
+  color: #337ab7;
+  cursor: pointer;
+  text-decoration: underline;
+  display: inline;
+  padding: 0;
+`
+
+export const PrintAndHomeContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
