6ad276e16e594df6fb6a718c26a7b9bafdf76cfd
eskokado <eskokado@gmail.com>
2023-05-27 14:36:04 -0300

    feat: add table by phone customer and contact

--- src/pages/CustomerPrintPage/components/CustomerToPrint/index.jsx
+++ src/pages/CustomerPrintPage/components/CustomerToPrint/index.jsx
@@ -9,7 +9,7 @@ export class CustomerToPrint extends React.Component {
     return (
       <Wrapper>
         <Cab>
-          <StyledParagraph>Customer</StyledParagraph>
+          <StyledParagraph>Cliente</StyledParagraph>
         </Cab>
         <Subcab>
           <StyledParagraph>Nome: {customer.name} </StyledParagraph>
@@ -20,22 +20,61 @@ export class CustomerToPrint extends React.Component {
           <StyledTable>
             <thead>
             <tr>
+              <th colSpan={2}>Telefones do Cliente</th>
+            </tr>
+            <tr>
+              <th>Nome</th>
+              <th>Número</th>
+            </tr>
+            </thead>
+            <tbody>
+            {customer.phones.map(phone => (
+              <tr key={phone.id}>
+                <td>{phone.name}</td>
+                <td>{phone.number}</td>
+              </tr>
+            ))}
+            </tbody>
+          </StyledTable>
+          <br/>
+          <br/>
+          <StyledTable>
+            <thead>
+            <tr>
+              <th colSpan={3}>Dados do contato</th>
+            </tr>
+            <tr>
               <th>Nome</th>
               <th>Email</th>
-              <th>Telefones</th>
+              <th>Telefones do Contato</th>
             </tr>
             </thead>
             <tbody>
             {customer.contacts.map(contact => (
-              <tr key={contact.id}>
-                <td>{contact.name}</td>
-                <td>{contact.email}</td>
-                <td>
-                  {contact.phones.map(phone => (
-                    <p key={phone.id}>{phone.number}</p>
-                  ))}
-                </td>
-              </tr>
+              <React.Fragment key={contact.id}>
+                <tr>
+                  <td>{contact.name}</td>
+                  <td>{contact.email}</td>
+                  <td>
+                    <StyledTable>
+                      <thead>
+                      <tr>
+                        <th>Nome</th>
+                        <th>Número</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      {contact.phones.map(phone => (
+                        <tr key={phone.id}>
+                          <td>{phone.name}</td>
+                          <td>{phone.number}</td>
+                        </tr>
+                      ))}
+                      </tbody>
+                    </StyledTable>
+                  </td>
+                </tr>
+              </React.Fragment>
             ))}
             </tbody>
           </StyledTable>
