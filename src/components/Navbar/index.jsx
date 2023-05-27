8cf3a1761c80d66e7cb10966221938a0241ddb7d
eskokado <eskokado@gmail.com>
2023-05-27 10:08:06 -0300

    fix: add button onPrint into component NavBar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,32 +1,42 @@
-import { Button, ButtonLink } from "../../styles/buttons";
-import logo from "../../assets/Logo.png";
-import { Logo } from "../../styles/logo";
-import { StyledNavbar } from "./styles";
+import { Button, ButtonLink } from '../../styles/buttons'
+import logo from '../../assets/Logo.png'
+import { Logo } from '../../styles/logo'
+import { StyledNavbar } from './styles'
 
-export const Navbar = ({ labelButton, to, onClick }) => {
+export const Navbar = ({ labelButton, labelPrint, to, onClick, onPrint }) => {
   return (
     <StyledNavbar>
       <Logo src={logo} />
       {to && (
         <ButtonLink
-          buttoncolor="grey"
-          buttonstyle="small"
-          fontSize="min"
+          buttoncolor='grey'
+          buttonstyle='small'
+          fontSize='min'
           to={to}
         >
           {labelButton}
         </ButtonLink>
       )}
+      {onPrint && (
+        <Button
+          buttoncolor='grey'
+          buttonstyle='small'
+          fontSize='min'
+          onClick={onPrint}
+        >
+          {labelPrint}
+        </Button>
+      )}
       {onClick && (
         <Button
-          buttoncolor="grey"
-          buttonstyle="small"
-          fontSize="min"
+          buttoncolor='grey'
+          buttonstyle='small'
+          fontSize='min'
           onClick={onClick}
         >
           {labelButton}
         </Button>
       )}
     </StyledNavbar>
-  );
-};
+  )
+}
