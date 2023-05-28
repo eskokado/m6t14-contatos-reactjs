540742ef6fd874dfc3ac4373da18623560e5f17a
eskokado <eskokado@gmail.com>
2023-05-28 08:34:45 -0300

    feat: create component FormPhoneContactModalHeader

---
+++ src/pages/HomePage/components/FormContactModal/FormContactModalContent/FormPhoneContactModal/FormPhoneContactModalHeader/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledFormPhoneContactModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 1rem;
+  padding: 0 2rem 0 1rem;
+`
