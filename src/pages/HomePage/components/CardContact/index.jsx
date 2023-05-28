7f5648dac740cb130ea717b7e4fbf7069fb797cb
eskokado <eskokado@gmail.com>
2023-05-28 07:16:07 -0300

    fix: rename property and add new properties into CardContact

--- src/pages/HomePage/components/CardContact/index.jsx
+++ src/pages/HomePage/components/CardContact/index.jsx
@@ -4,11 +4,14 @@ import { FaTrash } from 'react-icons/fa'
 import { Button } from '../../../../styles/buttons'
 import { useContext } from 'react'
 import { ContactContext } from '../../../../contexts/ContactContext'
+import {PhoneContactContext} from "../../../../contexts/PhoneContactContext";
 
-export const CardContact = ({ contact }) => {
-  const { setShowContactModal, setContact } = useContext(ContactContext)
+export const CardContact = ({ cardContact }) => {
+  const { setShowContactModal, setContact, contact } = useContext(ContactContext)
+  const { phonesContact, setPhonesContact } = useContext(PhoneContactContext)
   const handleShowModal = () => {
-    setContact(contact)
+    setPhonesContact(cardContact.phones)
+    setContact(cardContact)
     setShowContactModal(true)
   }
 
@@ -16,10 +19,10 @@ export const CardContact = ({ contact }) => {
     <StyledCardContact onClick={handleShowModal}>
       <div>
         <Typography fonttype='title3' fontcolor='grey0'>
-          {contact.name}
+          {cardContact.name}
         </Typography>
         <Typography fonttype='headline' fontcolor='grey1'>
-          {contact.email}
+          {cardContact.email}
         </Typography>
       </div>
     </StyledCardContact>
