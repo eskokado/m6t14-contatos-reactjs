65fbb6e8e862e846bcd0922a6b698ba3903c5f10
eskokado <eskokado@gmail.com>
2023-05-28 12:59:26 -0300

    feat: create component MainContentPhoneCustomer

---
+++ src/pages/HomePage/components/FormCustomerModal/MainContentPhoneCustomer/index.jsx
@@ -0,0 +1,10 @@
+import { StyledMainContentPhoneCustomer} from './styles'
+import {CardListPhoneCustomer} from ".././CardListPhoneCustomer";
+
+export const MainContentPhoneCustomer = () => {
+  return (
+    <StyledMainContentPhoneCustomer>
+      <CardListPhoneCustomer />
+    </StyledMainContentPhoneCustomer>
+  )
+}
