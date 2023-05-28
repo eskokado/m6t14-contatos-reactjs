815d9d2ec6ab05a247c38dffaf5545b3054bdff9
eskokado <eskokado@gmail.com>
2023-05-28 12:58:31 -0300

    feat: create component FormCustomerModalHeader

---
+++ src/pages/HomePage/components/FormCustomerModal/FormCustomerModalHeader/index.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react'
+import { CustomerContext } from '../../../../../contexts/CustomerContext'
+import { Button } from '../../../../../styles/buttons'
+import { Typography } from '../../../../../styles/typography'
+import { StyledFormCustomerModalHeader } from './styles'
+
+export const FormCustomerModalHeader = () => {
+  const { setShowCustomerModal, tech } = useContext(CustomerContext)
+  return (
+    <StyledFormCustomerModalHeader>
+      <Typography fonttype='title3' fontcolor='grey0' fontweight='semibold'>
+        {tech ? 'Cliente Detalhes' : 'Cadastrar Cliente'}
+      </Typography>
+      <Button buttoncolor='grey1' onClick={() => setShowCustomerModal(false)}>
+        X
+      </Button>
+    </StyledFormCustomerModalHeader>
+  )
+}
