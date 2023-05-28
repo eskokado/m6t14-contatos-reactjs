7f1ee4c5f1efcc33479181d87950655b9ee0107b
eskokado <eskokado@gmail.com>
2023-05-28 07:05:59 -0300

    feat: create component MainTopPhoneContact

---
+++ src/pages/HomePage/components/FormContactModal/MainTopPhoneContact/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledMainTopPhoneContact = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+  align-items: center;
+`;
