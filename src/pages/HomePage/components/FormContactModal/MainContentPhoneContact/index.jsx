eec2195e1573a90491f21da9d6fe64315d3e749e
eskokado <eskokado@gmail.com>
2023-05-28 07:06:25 -0300

    feat: create component MainContentPhoneContact

---
+++ src/pages/HomePage/components/FormContactModal/MainContentPhoneContact/index.jsx
@@ -0,0 +1,10 @@
+import { StyledMainContentPhoneContact} from './styles'
+import {CardListPhoneContact} from "../CardListPhoneContact";
+
+export const MainContentPhoneContact = () => {
+  return (
+    <StyledMainContentPhoneContact>
+      <CardListPhoneContact />
+    </StyledMainContentPhoneContact>
+  )
+}
